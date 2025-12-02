68a5126b61e4d758c48a3afaa0fd838b71c8c22c
shaelyn-l <86339248+shaelyn-l@users.noreply.github.com>
2025-12-01 17:25:56 -1000

    remove auth and db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,54 +5,10 @@ import { Container, Row, Col, Form } from 'react-bootstrap';
 import Environment_Human from '../components/Environment_Human';
 
 export default function HomePage() {
-  // const [showPlants, setShowPlants] = useState(true);
-  // const [showCriticalHabitat, setShowCriticalHabitat] = useState(false);
 
   return (
     <main>
       <Environment_Human />
     </main>
-    // <main>
-    //   <Container fluid className="py-3">
-    //     <Row>
-    //       {/* Visualization */}
-    //       <Col xs={9}>
-    //         <PlotlyMap
-    //           showPlants={showPlants}
-    //           showCriticalHabitat={showCriticalHabitat}
-    //         />
-    //       </Col>
-
-    //       {/* Sidebar */}
-    //       <Col
-    //         xs={3}
-    //         style={{
-    //           borderLeft: '1px solid #ccc',
-    //           paddingLeft: '20px',
-    //           height: '100vh',
-    //         }}
-    //       >
-    //         <h4>Layers</h4>
-    //         <Form>
-    //           <Form.Check
-    //             type="checkbox"
-    //             id="plants"
-    //             label="Threatened & Endangered Plants"
-    //             checked={showPlants}
-    //             onChange={(e) => setShowPlants(e.target.checked)}
-    //           />
-
-    //           <Form.Check
-    //             type="checkbox"
-    //             id="criticalHabitat"
-    //             label="Critical Habitat Areas"
-    //             checked={showCriticalHabitat}
-    //             onChange={(e) => setShowCriticalHabitat(e.target.checked)}
-    //           />
-    //         </Form>
-    //       </Col>
-    //     </Row>
-    //   </Container>
-    // </main>
   );
 }
