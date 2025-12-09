8cbaf93f72703993bf22f1568b851e2bfdb2e54e
shaelyn-l <86339248+shaelyn-l@users.noreply.github.com>
2025-12-09 12:57:35 -1000

    idk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,15 @@ import './globals.css';
 import type { ReactNode } from 'react';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Human & Environment Visualization',
+  description: 'Final project for ICS 484',
+};
 
 export default function RootLayout({
   children,
